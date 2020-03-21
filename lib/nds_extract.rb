41dcb5de888b93b16cc76170bd38cd8014d25b51
Steven Parsons <steven.parsons.us@gmail.com>
2020-03-21 21:30:21 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,13 +5,12 @@ def directors_totals(nds)
   directors_totals = {}
   nameSearch = 0
   while nameSearch < nds.count
-  #nds.count = the amount of directors in the array
-  directorName = nds[nameSearch][:name]
+   directorName = nds[nameSearch][:name]
    moviesCombinedTotal = 0
    movieSearch = 0 
    while movieSearch < nds[nameSearch][:movies].count
-    movieGross = nds[n][:movies][g][:worldwide_gross]
-    moviesCombinedTotal += movieGross
+    oneMovieGross = nds[n][:movies][g][:worldwide_gross]
+    moviesCombinedTotal += oneMovieGrossmovieGross
     movieSearch += 1
     end
     directors_totals[directorName] = moviesCombinedTotal
