bfd858ec619d60b523f4b90eee086b019fa74db4
Steven Parsons <steven.parsons.us@gmail.com>
2020-03-21 21:57:29 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -3,18 +3,5 @@ require 'directors_database'
 
 def directors_totals(nds)
   directors_totals = {}
-  nameSearch = 0
-  while nameSearch < nds.count
-   directorName = nds[nameSearch][:name]
-   moviesCombinedTotal = 0
-   movieSearch = 0 
-   while movieSearch < nds[nameSearch][:movies].count
-   movieGross = nds[nameSearch][:movies][movieSearch][:worldwide_gross]
-    moviesCombinedTotal += movieGross
-    movieSearch += 1
-    end
-    directors_totals[directorName] = moviesCombinedTotal
-    nameSearch += 1
-  end
-  directors_totals
+  pp nds
 end
