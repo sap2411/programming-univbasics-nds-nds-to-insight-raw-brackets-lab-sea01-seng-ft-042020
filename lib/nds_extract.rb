47bbbf967a6f15cd6f30d32bb45beed5848766a0
Steven Parsons <steven.parsons.us@gmail.com>
2020-03-21 21:32:42 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -10,7 +10,7 @@ def directors_totals(nds)
    movieSearch = 0 
    while movieSearch < nds[nameSearch][:movies].count
    movieGross = nds[nameSearch][:movies][movieSearch][:worldwide_gross]
-    moviesCombinedTotal += oneMovieGross
+    moviesCombinedTotal += movieGross
     movieSearch += 1
     end
     directors_totals[directorName] = moviesCombinedTotal
